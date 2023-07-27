e756bd8ea9c21501916e044667baac9f91b222af
stilinskii <lhy1031hy@gmail.com>
2023-07-28 00:06:10 +0900

    [Linked list] set

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -141,4 +141,14 @@ public class LinkedList {
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+
 }
