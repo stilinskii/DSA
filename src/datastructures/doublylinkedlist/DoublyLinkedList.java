aaabcd628cfb3e5ad91225dd14db556d0a4c224a
stilinskii <lhy1031hy@gmail.com>
2023-08-03 00:10:22 +0900

    [Doubly Linked list] get

--- src/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -119,4 +119,21 @@ public class DoublyLinkedList {
         return temp;
     }
 
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        if (index < length/2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            //index starts from 0 , so length - 1
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
 }
